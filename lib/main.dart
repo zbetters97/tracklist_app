246f19a720fea7d9dd133c03cf8f68be90b0b371
Zachary Betters <zachbetters97@gmail.com>
2025-10-08 14:09:15 -0400

    Alpha v0.0.4 -Adds launcher icons

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:tracklist_app/data/constants.dart';
 import 'package:tracklist_app/services/firebase_options.dart';
-import 'package:tracklist_app/services/spotify_search.dart';
 import 'package:tracklist_app/views/widget_tree.dart';
 
 void main() async {
@@ -13,45 +12,6 @@ void main() async {
   runApp(const MyApp());
 }
 
-void searchDemo() async {
-  final List<Map<String, dynamic>> albums = await searchAlbums(album: 'Landmark');
-  print('Search for: "Landmark":');
-  for (final Map<String, dynamic> album in albums) {
-    print('${album['name']} by ${album['artist']} [${album['release_date']}] Cover: ${album['image']}');
-  }
-
-  final List<Map<String, dynamic>> artists = await searchArtists(artist: 'Hippo Campus');
-  print('Search for: "Hippo Campus":');
-  for (final Map<String, dynamic> artist in artists) {
-    print('${artist['name']}, Cover: ${artist['image']}');
-  }
-
-  final List<Map<String, dynamic>> artist = await searchArtists(artist: 'Hippo Campus');
-  String artistId = artist[0]['id'] as String;
-
-  final List<Map<String, dynamic>> artistAlbums = await getArtistAlbums(artistId: artistId);
-  artistAlbums.sort((a, b) => a['release_date'].compareTo(b['release_date']));
-
-  print("Albums by ${artist[0]['name']}:");
-  for (final Map<String, dynamic> album in artistAlbums) {
-    print('[${album['release_date']}] ${album['name']}: Cover: ${album['image']}');
-  }
-
-  final List<Map<String, dynamic>> album = await searchAlbums(album: 'Landmark');
-  String albumId = album[0]['id'] as String;
-
-  final List<Map<String, dynamic>> albumTracks = await getAlbumTracks(albumId);
-  albumTracks.sort((a, b) => a['track_number'].compareTo(b['track_number']));
-
-  print("Tracks from ${album[0]['name']}:");
-  for (final Map<String, dynamic> track in albumTracks) {
-    double minutes = track['duration'] ~/ 1000 / 60;
-    print('[${track['track_number']}] ${track['name']}: ${minutes.toStringAsFixed(2)} mins');
-  }
-}
-
-// Stateless: screen cannot be changed (can't refresh/update)
-// Stateful: screen can be changed (can refresh/update)
 class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
@@ -63,7 +23,7 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Spotify Demo',
+      title: 'TrackList',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: PRIMARY_COLOR, brightness: Brightness.dark),
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -11,6 +11,7 @@ import firebase_auth
 import firebase_core
 import flutter_web_auth_2
 import path_provider_foundation
+import shared_preferences_foundation
 import url_launcher_macos
 import window_to_front
 
@@ -21,6 +22,7 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   FlutterWebAuth2Plugin.register(with: registry.registrar(forPlugin: "FlutterWebAuth2Plugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
+  SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
   WindowToFrontPlugin.register(with: registry.registrar(forPlugin: "WindowToFrontPlugin"))
 }
